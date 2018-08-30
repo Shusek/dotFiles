d57b7dfa9c92a84154c751761be50cd00048ad73
m.suska <m.suska@partner.samsung.com>
2018-08-30 15:46:31 +0200

    Fix: Android emulator should be floating window

--- i3/someScript.sh
+++ i3/someScript.sh
@@ -1,11 +1,11 @@
 #!/bin/sh
 
-TITLES=$(swaymsg -t get_tree | jq '.. | objects | select(.nodes? == []) | 
+TITLES=$(swaymsg -t get_tree | jq -r '.. | objects | select(.nodes? == []) | 
 (.id|tostring)+" "+.window_properties.class + "   " + .window_properties.title')
 echo $TITLES
-CHOSEN=$(grep '"*"'<<< $TITLES |dmenu -i -fn 'fira-10' -l 10)
-echo $CHOSEN
-echo $(echo $TITLES | grep 'Fire' | awk '{print $1}' ) 
+#CHOSEN=$(grep '"*"'<<< $TITLES |dmenu -i -fn 'fira-10' -l 10)
+#echo $CHOSEN
+#echo $(echo $TITLES | grep 'Fire' | awk '{print $1}' ) 
 #TITLES  | dmenu -i -fn 'fira-10' -l 10
 
 
